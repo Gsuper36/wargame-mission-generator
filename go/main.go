a01850202c29f874a743e1b19e6432785923e3f8
Daniil Budarnyy <gsuper36@gmail.com>
2021-11-07 23:25:31 +0300

    Add env support for go module

--- go/main.go
+++ go/main.go
@@ -1,8 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"fmt"
+	"github.com/joho/godotenv"
+)
 
 // Здесь будем читать конфиг и запускать сервис
 func main() {
-	fmt.Print("This is go-code stub!")
+	loadEnv()
+
+	serverPort := os.Getenv("GENERATOR_PORT")
+
+	fmt.Print(serverPort)
+}
+
+func loadEnv() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		panic(err)
+	}
 }
